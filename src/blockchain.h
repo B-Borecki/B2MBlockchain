80dcf5d15912d7d455abb8cf391403ff5480974a
Mikolkar <mikolaj.karapka@gmail.com>
2024-11-18 16:14:13 +0100

    Added class Block, Updated main.cpp and .gitignore

--- src/blockchain.h
+++ src/blockchain.h
@@ -1,17 +1,37 @@
-#ifndef BLOCKCHAIN_H
-#define BLOCKCHAIN_H
+#pragma once
 
-#include <string>
 #include <iostream>
+#include <string>
+#include <vector>
 
-class Transaction {
-private:
+namespace Blockchain
+{
+
+  class Transaction
+  {
+  private:
     std::string sender_id;
     std::string receiver_id;
     double amount;
-public:
+
+  public:
     Transaction(std::string sender, std::string receiver, double amt);
     void print() const;
-};
+  };
 
-#endif
+  class Block
+  {
+  private:
+    static int count_id_block;
+
+  public:
+    int id_block;
+    int id_prev;
+    std::vector<Transaction> t_actions_lst;
+
+    Block();
+    Block(std::vector<Transaction> &t_actions_lst_argv);
+
+    void print();
+  };
+}
